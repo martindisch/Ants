8c54c2e5fd99624b5b9553397a3e05ca39fda7e8
Martin Disch <martindisch@gmail.com>
2014-11-05 09:24:53 +0100

    Removed unncecessary comments

--- mSugarAnts.cs
+++ mSugarAnts.cs
@@ -3,25 +3,14 @@ using System.Collections.Generic;
 
 using AntMe.Deutsch;
 
-// Füge hier hinter AntMe.Spieler einen Punkt und deinen Namen ohne Leerzeichen
-// ein! Zum Beispiel "AntMe.Spieler.WolfgangGallo".
 namespace AntMe.Spieler
 {
-
-	// Das Spieler-Attribut erlaubt das Festlegen des Volk-Names und von Vor-
-	// und Nachname des Spielers. Der Volk-Name muß zugewiesen werden, sonst wird
-	// das Volk nicht gefunden.
 	[Spieler(
 		Volkname = "mSugarAnts",
 		Vorname = "Martin",
 		Nachname = "Disch"
 	)]
 
-	// Das Typ-Attribut erlaubt das Ändern der Ameisen-Eigenschaften. Um den Typ
-	// zu aktivieren muß ein Name zugewiesen und dieser Name in der Methode 
-	// BestimmeTyp zurückgegeben werden. Das Attribut kann kopiert und mit
-	// verschiedenen Namen mehrfach verwendet werden.
-	// Eine genauere Beschreibung gibts in Lektion 6 des Ameisen-Tutorials.
 	[Kaste(
 		Name = "Standard",
 		GeschwindigkeitModifikator = 1,
@@ -38,12 +27,6 @@ namespace AntMe.Spieler
 
 		#region Kaste
 
-		/// <summary>
-		/// Bestimmt die Kaste einer neuen Ameise.
-		/// </summary>
-		/// <param name="anzahl">Die Anzahl der von jeder Kaste bereits vorhandenen
-		/// Ameisen.</param>
-		/// <returns>Der Name der Kaste der Ameise.</returns>
 		public override string BestimmeKaste(Dictionary<string, int> anzahl)
 		{
 			return "Standard";
@@ -53,10 +36,6 @@ namespace AntMe.Spieler
 
 		#region Fortbewegung
 
-		/// <summary>
-		/// Wird wiederholt aufgerufen, wenn der die Ameise nicht weiss wo sie
-		/// hingehen soll.
-		/// </summary>
 		public override void Wartet()
 		{
             if (Ziel == null)
@@ -66,10 +45,6 @@ namespace AntMe.Spieler
             }
 		}
 
-		/// <summary>
-		/// Wird einmal aufgerufen, wenn die Ameise ein Drittel ihrer maximalen
-		/// Reichweite überschritten hat.
-		/// </summary>
 		public override void WirdMüde()
 		{
             GeheZuBau();
@@ -79,11 +54,6 @@ namespace AntMe.Spieler
 
 		#region Nahrung
 
-		/// <summary>
-		/// Wird wiederholt aufgerufen, wenn die Ameise mindestens einen
-		/// Zuckerhaufen sieht.
-		/// </summary>
-		/// <param name="zucker">Der nächstgelegene Zuckerhaufen.</param>
 		public override void Sieht(Zucker zucker)
 		{
             if (AktuelleLast == 0)
@@ -92,11 +62,6 @@ namespace AntMe.Spieler
             }
 		}
 
-		/// <summary>
-		/// Wird wiederholt aufgerufen, wenn die Ameise mindstens ein
-		/// Obststück sieht.
-		/// </summary>
-		/// <param name="obst">Das nächstgelegene Obststück.</param>
 		public override void Sieht(Obst obst)
 		{
             if (BrauchtNochTräger(obst))
@@ -105,23 +70,15 @@ namespace AntMe.Spieler
             }
 		}
 
-		/// <summary>
-		/// Wird einmal aufgerufen, wenn di e Ameise einen Zuckerhaufen als Ziel
-		/// hat und bei diesem ankommt.
-		/// </summary>
-		/// <param name="zucker">Der Zuckerhaufen.</param>
 		public override void ZielErreicht(Zucker zucker)
 		{
+            // Spray a very large marking, which will be gone very quickly
+            // but picked up by a large number of ants.
             SprüheMarkierung(1, 2000);
             Nimm(zucker);
             GeheZuBau();
 		}
 
-		/// <summary>
-		/// Wird einmal aufgerufen, wenn die Ameise ein Obststück als Ziel hat und
-		/// bei diesem ankommt.
-		/// </summary>
-		/// <param name="obst">Das Obstück.</param>
 		public override void ZielErreicht(Obst obst)
 		{
             if (BrauchtNochTräger(obst))
@@ -136,12 +93,6 @@ namespace AntMe.Spieler
 
 		#region Kommunikation
 
-		/// <summary>
-		/// Wird einmal aufgerufen, wenn die Ameise eine Markierung des selben
-		/// Volkes riecht. Einmal gerochene Markierungen werden nicht erneut
-		/// gerochen.
-		/// </summary>
-		/// <param name="markierung">Die nächste neue Markierung.</param>
 		public override void RiechtFreund(Markierung markierung)
 		{
             if (Ziel == null)
@@ -150,19 +101,10 @@ namespace AntMe.Spieler
             }
 		}
 
-		/// <summary>
-		/// Wird wiederholt aufgerufen, wenn die Ameise mindstens eine Ameise des
-		/// selben Volkes sieht.
-		/// </summary>
-		/// <param name="ameise">Die nächstgelegene befreundete Ameise.</param>
 		public override void SiehtFreund(Ameise ameise)
 		{
 		}
 
-		/// <summary>
-		/// Wird aufgerufen, wenn die Ameise eine befreundete Ameise eines anderen Teams trifft.
-		/// </summary>
-		/// <param name="ameise"></param>
 		public override void SiehtVerbündeten(Ameise ameise)
 		{
 		}
@@ -171,11 +113,6 @@ namespace AntMe.Spieler
 
 		#region Kampf
 
-		/// <summary>
-		/// Wird wiederholt aufgerufen, wenn die Ameise mindestens eine Wanze
-		/// sieht.
-		/// </summary>
-		/// <param name="wanze">Die nächstgelegene Wanze.</param>
 		public override void SiehtFeind(Wanze wanze)
 		{
             // Check if we collide with the bug
@@ -196,29 +133,14 @@ namespace AntMe.Spieler
             }
 		}
 
-		/// <summary>
-		/// Wird wiederholt aufgerufen, wenn die Ameise mindestens eine Ameise eines
-		/// anderen Volkes sieht.
-		/// </summary>
-		/// <param name="ameise">Die nächstgelegen feindliche Ameise.</param>
 		public override void SiehtFeind(Ameise ameise)
 		{
 		}
 
-		/// <summary>
-		/// Wird wiederholt aufgerufen, wenn die Ameise von einer Wanze angegriffen
-		/// wird.
-		/// </summary>
-		/// <param name="wanze">Die angreifende Wanze.</param>
 		public override void WirdAngegriffen(Wanze wanze)
 		{
 		}
 
-		/// <summary>
-		/// Wird wiederholt aufgerufen in der die Ameise von einer Ameise eines
-		/// anderen Volkes Ameise angegriffen wird.
-		/// </summary>
-		/// <param name="ameise">Die angreifende feindliche Ameise.</param>
 		public override void WirdAngegriffen(Ameise ameise)
 		{
 		}
@@ -227,17 +149,10 @@ namespace AntMe.Spieler
 
 		#region Sonstiges
 
-		/// <summary>
-		/// Wird einmal aufgerufen, wenn die Ameise gestorben ist.
-		/// </summary>
-		/// <param name="todesart">Die Todesart der Ameise</param>
 		public override void IstGestorben(Todesart todesart)
 		{
 		}
 
-		/// <summary>
-		/// Wird unabhängig von äußeren Umständen in jeder Runde aufgerufen.
-		/// </summary>
 		public override void Tick()
 		{
 		}
